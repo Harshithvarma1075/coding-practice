5f0664b9014fd9f281c7263018ac69947a8c740e
harshith varma <harshithvarmaganapathiraju@gmail.com>
2026-04-13 13:01:57 +0530

    generators.py

---
+++ generators.py
@@ -0,0 +1,32 @@
+':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::GENERATORS:::::::::::::::::::::::::::::::::::::::::::::::::::::::'
+'THIS IS A SPECIAL TYPE OF FUNCTION THAT GENERATES OR RETURN AN ITERATOR WHICH ONE AT A TIME'
+'''
+def my_gen():
+    yield 1
+    yield 2
+    yield 3
+an = my_gen()
+print(next(an))  'NEXT : this is used to get next value from a generator and when the value is finished it will stop the iterator'
+print(next(an))
+print(next(an))
+
+'''
+':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::SQUARE GEN::::::::::::::::::::::::::::::::::::::::::::::::::::::'
+'''
+def squares(n):
+    for i in range(n):
+        yield i*i
+for num in squares(10):
+    print(num)
+'''
+'YIELD GIVES THE OUTPUT WITH A PAUSE OF MILLI SECOND  , IT IS SIMILAR TO RETURN BUT RETURN GIVES OUTPUT AT A TIME '
+
+'::::::::::::::::::::::::::::::::::::::::::::::::POWER GEN::::::::::::::::::::::::::::::::::::::::::'
+'''
+def power_gen(n):
+    for i in range(1,n+1):
+        yield i**i
+for num in power_gen(10):
+    print(num)
+'''
+':::::::::::::::::::::::::::::::::::::::::::::::::
